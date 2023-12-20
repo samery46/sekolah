33688c54fcdd265c0ee9006cb4931166c8bb7607
samery46 <syamme@ymail.com>
2023-12-20 16:49:59 +0700

    Website Sekolah

--- resources/views/frontend/content/guru.blade.php
+++ resources/views/frontend/content/guru.blade.php
@@ -26,11 +26,11 @@
                                             class="fa fa-globe" aria-hidden="true"></i></a></li>
                                 <li><a href="mailto:{{ $pengajars->userDetail->email }}"><i class="fa fa-envelope-o"
                                             aria-hidden="true"></i></a></li>
-                                <li><a href="https://wa.me/{{ $pengajars->userDetail->linkedln }}" target="_blank"><i
+                                <li><a href="https://wa.me/{{ $pengajars->userDetail->linkidln }}" target="_blank"><i
                                             class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
-                                <li><a href="{{ 'https://www.twitter.com/', $pengajars->userDetail->twitter }}"
+                                <li><a href="https://www.twitter.com/{{ $pengajars->userDetail->twitter }}"
                                         target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                <li><a href="{{ 'https://www.facebook.com/', $pengajars->userDetail->facebook }}"
+                                <li><a href="https://www.facebook.com/{{ $pengajars->userDetail->facebook }}"
                                         target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                                 <li><a href="https://www.instagram.com/{{ $pengajars->userDetail->instagram }}"
                                         target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
