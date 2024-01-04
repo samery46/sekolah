cebac50255847df67e8e580663e2808147d03c16
samery46 <syamme@ymail.com>
2024-01-04 09:47:57 +0700

    Update kegiatan

--- resources/views/frontend/program/jurusan/show.blade.php
+++ resources/views/frontend/program/jurusan/show.blade.php
@@ -10,13 +10,13 @@
             <div class="row about-page1-inner">
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                     <div class="about-page-content-holder">
-                        {!! $jurusan->dataJurusan->content !!}
+                        <p> {!! $jurusan->dataJurusan->content !!} </p>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                     <div class="about-page-img-holder">
-                        <img src="{{ asset('storage/images/jurusan/' . $jurusan->dataJurusan->image) }}" class="img-responsive"
-                            alt="about">
+                        <img src="{{ asset('storage/images/jurusan/' . $jurusan->dataJurusan->image) }}"
+                            class="img-responsive" alt="about">
                     </div>
                 </div>
             </div>
--- resources/views/frontend/program/kegiatan/show.blade.php
+++ resources/views/frontend/program/kegiatan/show.blade.php
@@ -10,7 +10,7 @@
             <div class="row about-page1-inner">
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                     <div class="about-page-content-holder">
-                        {!! $kegiatan->content !!}
+                        <p> {!! $kegiatan->content !!} </p>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
